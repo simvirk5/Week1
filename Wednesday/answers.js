c1eea21d4c1ceae51cb242ed442c4fd560106f2c
simvirk5 <simvirk5@gmail.com>
2018-03-21 12:58:36 -0700

    week 1

--- Wednesday/answers.js
+++ Wednesday/answers.js
@@ -1,4 +1,36 @@
-var i = 0
-while (i<=200) {
+// Get Even
+// // var i = 0
+// // while (i<=200) {
+// // 	console.log(i++);
+// // }
+
+// Excited Kitten
+
+// for(var i=1; i<=20; i++) {
+// 	console.log("Love me, pet!HSSSSSS!");
+// }
+
+ 
+//  function evenNumbers(n) { 
+//  for(var i=0; i<=20; i+=2) {
+// 	evenNumbers.push[Math.random("human..why you taking pictures of me?.." || 
+// 		"..the catnip made me do it.." || 
+// 		"..why does the red dot always get away")];
+
+// }
+
+// 	return var;
+// }
+
+// Fizz Buzz
+
+var i = 0;
+while(i<=100) {
 	console.log(i++);
+}
+
+for(var i=0; i<=100; i++) {
+	if (i%3===0) {
+		console.log(i,"Fizz");
+	}
 }
