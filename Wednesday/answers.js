f9f82c5ba26761836451a6024af7f203ed7bddaa
simvirk5 <simvirk5@gmail.com>
2018-03-21 16:35:05 -0700

    Commit 5- Getting to Know you answered

--- Wednesday/answers.js
+++ Wednesday/answers.js
@@ -48,6 +48,18 @@ for(var i=0; i<=100; i++) {
 	}
 }
 
+Getting to Know you
+
+const thom = ["Thom", 1000, "Christchurch"]
+const karolin = ["Karolin", 16, "New York"]
+const kristyn = ["Kristyn", 5, "Pittsburgh"]
+const matt = ["Matt H", 186, "Philadelphia"]
+
+thom[0] = 'Gameboy'
+karolin [1] = 17
+matt[2] = 'Gotham City'
+kristyn[2] = 'Brooklyn'
+
 
 
 
