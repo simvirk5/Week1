9b16e60301073f282145c16f6c6c57ef93c17c8e
simvirk5 <simvirk5@gmail.com>
2018-03-21 16:32:55 -0700

    Commit 1-4

--- Wednesday/answers.js
+++ Wednesday/answers.js
@@ -1,28 +1,29 @@
-// Get Even
-// // var i = 0
-// // while (i<=200) {
-// // 	console.log(i++);
-// // }
+Get Even
 
-// Excited Kitten
+ var i = 0
+ while (i<=200) {
+ 	console.log(i++);
+ }
 
-// for(var i=1; i<=20; i++) {
-// 	console.log("Love me, pet!HSSSSSS!");
-// }
+Excited Kitten
 
- 
+for(var i=1; i<=20; i++) {
+	console.log("Love me, pet!HSSSSSS!");
+}
+
+
 //  function evenNumbers(n) { 
-//  for(var i=0; i<=20; i+=2) {
-// 	evenNumbers.push[Math.random("human..why you taking pictures of me?.." || 
-// 		"..the catnip made me do it.." || 
-// 		"..why does the red dot always get away")];
+//   for(var i=0; i<=20; i+=2) {
+//  	arr.push[Math.random("human..why you taking pictures of me?.." || 
+//  		"..the catnip made me do it.." || 
+//  		"..why does the red dot always get away")];
 
 // }
 
 // 	return var;
 // }
 
-// Fizz Buzz
+Fizz Buzz
 
 var i = 0;
 while(i<=100) {
@@ -34,3 +35,19 @@ for(var i=0; i<=100; i++) {
 		console.log(i,"Fizz");
 	}
 }
+
+for(var i=0; i<=100; i++) {
+	if (i%5===0) {
+		console.log(i,"Buzz");
+	}
+}
+
+for(var i=0; i<=100; i++) {
+	if (i%3===0 && i%5===0) {
+		console.log(i,"FizzBuzz");
+	}
+}
+
+
+
+
